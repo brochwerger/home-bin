e1a18558aa77fedf5f208a980ceef3276efa52ac
Benny Rochwerger <brochwer@redhat.com>
2022-08-02 13:33:37 +0300

    Support different params for yaffo and poas

--- camera-setup.sh
+++ camera-setup.sh
@@ -1,5 +1,20 @@
 #! /bin/sh
+# Preq: dnf install v4l2ucp v4l-utils
 
-v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=125
-v4l2-ctl -d /dev/video4 --set-ctrl tilt_absolute=-35000
-v4l2-ctl -d /dev/video4 --set-ctrl brightness=90
+location=$1
+
+#v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=125
+v4l2-ctl -d /dev/video4 --set-ctrl brightness=100
+
+if [ "$location" = "poas" ]
+then
+    v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=110
+    v4l2-ctl -d /dev/video4 --set-ctrl tilt_absolute=-36000
+elif [ "$location" = "yaffo" ]
+then
+    v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=115
+    v4l2-ctl -d /dev/video4 --set-ctrl tilt_absolute=-36000
+else
+    echo 'Usage: camera-setup yaffo|poas'
+fi
+
