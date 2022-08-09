df9c626c9e899b5e3564f070a9ae75f6344050b5
Benny Rochwerger <brochwer@redhat.com>
2022-08-09 17:32:08 +0300

    - Switched to options instead of positional parameters - Added option (-c) to select camera device - Added option (-g) to lauch graphical configurator

--- camera-setup.sh
+++ camera-setup.sh
@@ -1,20 +1,55 @@
 #! /bin/sh
 # Preq: dnf install v4l2ucp v4l-utils
 
-location=$1
+help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Usage: camera-setup -l <location> -c <camera> -g"
+}
 
-#v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=125
-v4l2-ctl -d /dev/video4 --set-ctrl brightness=100
+location="yaffo"
+camera="video4"
+
+while getopts ":l:c:gh" option; do
+   case $option in
+	l) location=$OPTARG;;
+	c) camera=$OPTARG;;
+    g) # Invoke graphical configurator
+        guvcview --control_panel -d /dev/$camera >& /dev/null
+        exit;;
+    h) 
+        help
+        exit;;
+    \?) # Invalid option
+        echo "Error: Invalid option"
+        exit;;
+   esac
+done
+
+echo $camera
+echo $location
+
+#v4l2-ctl -d /dev/$camera --set-ctrl zoom_absolute=125
+v4l2-ctl -d /dev/$camera --set-ctrl brightness=100
+v4l2-ctl -d /dev/$camera --set-ctrl saturation=128
+v4l2-ctl -d /dev/$camera --set-ctrl tilt_absolute=-36000
 
 if [ "$location" = "poas" ]
 then
-    v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=110
-    v4l2-ctl -d /dev/video4 --set-ctrl tilt_absolute=-36000
+    v4l2-ctl -d /dev/$camera --set-ctrl zoom_absolute=110
 elif [ "$location" = "yaffo" ]
 then
-    v4l2-ctl -d /dev/video4 --set-ctrl zoom_absolute=115
-    v4l2-ctl -d /dev/video4 --set-ctrl tilt_absolute=-36000
-else
-    echo 'Usage: camera-setup yaffo|poas'
+    v4l2-ctl -d /dev/$camera --set-ctrl zoom_absolute=115
+    v4l2-ctl -d /dev/$camera --set-ctrl backlight_compensation=1
+elif [ "$location" = "z-gila" ]
+then
+    v4l2-ctl -d /dev/$camera --set-ctrl zoom_absolute=125
+    v4l2-ctl -d /dev/$camera --set-ctrl brightness=150
+    v4l2-ctl -d /dev/$camera --set-ctrl tilt_absolute=-6000
+
+# else
+#     echo 'Usage: camera-setup yaffo|poas|z-gila'
 fi
 
